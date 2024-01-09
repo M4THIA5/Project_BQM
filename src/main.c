553163683b7d44a627e5d11187688222f7266837
Mathias Collas-Jourdan <mathias.collas@gmail.com>
2024-01-09 01:52:40 +0100

    Add text in window in progress

--- src/main.c
+++ src/main.c
@@ -4,19 +4,43 @@
 #define WINDOW_WIDTH 1080
 #define WINDOW_HEIGHT 720
 
-//rappel de compilation
 
-//Windows :   gcc src/main.c -o bin/main -I include -L lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf
+
+
+
+
+
+// erreur lors de l'importation de la font !!!
+
+// ligne 56
+
+
+
+
+
+
+
+// rappel de compilation
+
+// Windows :   gcc src/main.c -o bin/main -I include -L lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf
 
 void SDL_ExitWithError(const char* message);
 
 int main(int argc, char* argv[]){
 
 	SDL_Window* firstWindow = NULL;
-	SDL_Renderer* firstRenderer = NULL;
+	SDL_Renderer* renderer = NULL;
+
+	SDL_Surface* surface = NULL;
+	SDL_Texture* texture = NULL;
+	SDL_Rect rect = { 0, 0, 0, 0 }; 	// position.x , position.y , largeur , hauteur
+
+	TTF_Font* font = NULL;
+	SDL_Color color = { 255, 255, 255 };
+
 
 	//Lancement SDL
-	if(SDL_Init(SDL_INIT_VIDEO) != 0){ //renvoie 0 si tout va bien
+	if(SDL_Init(SDL_INIT_VIDEO) != 0){ 	// renvoie 0 si tout va bien
 		SDL_ExitWithError("initializing SDL");
 	}
 	SDL_Log("SDL successfully initialized\n");
@@ -27,11 +51,54 @@ int main(int argc, char* argv[]){
 	SDL_Log("TTF successfully initialized\n");
 
 	//Création fenêtre + rendu
-	if(SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &firstWindow, &firstRenderer) != 0){
+	if(SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &firstWindow, &renderer) != 0){
 		SDL_ExitWithError("Creating window & renderer failed");
 	}
 
+	font = TTF_OpenFont("../font/arial.ttf", 25);
+
+	if(font == NULL){
+		TTF_CloseFont(font);
+		SDL_ExitWithError("Add font failed");
+	}
+
 	/*-------------------------------------------------------*/
+	
+	surface = TTF_RenderText_Solid(font, "test test", color);
+	if(surface == NULL){
+		TTF_CloseFont(font);
+		SDL_FreeSurface(surface);
+		SDL_ExitWithError("Add surface failed");
+	}
+
+	// Une fois la texture crée, plus besoin de la font
+	TTF_CloseFont(font);
+
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+	// Une fois la texture crée, plus besoin de la surface
+	SDL_FreeSurface(surface);
+
+	if(texture == NULL){
+		SDL_ExitWithError("Add texture failed");
+	}
+
+	if(SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h)){
+		SDL_ExitWithError("texture loading failed");
+	}
+
+	rect.x = (WINDOW_WIDTH - rect.w) / 2;
+
+	if(SDL_RenderCopy(renderer, texture, NULL, &rect) != 0){
+		SDL_ExitWithError("print message failed");
+	}
+
+	/*-------------------------------------------------------*/
+
+	SDL_RenderPresent(renderer);
+	SDL_Delay(5000);
+
+	
 	SDL_bool run = SDL_TRUE;	// Struct booleenne True/False
 
 	//Boucle infini
@@ -60,8 +127,11 @@ int main(int argc, char* argv[]){
 	}
 
 	/*-------------------------------------------------------*/
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(surface);
+	TTF_Quit();
 
-	SDL_DestroyRenderer(firstRenderer);
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(firstWindow);
 	SDL_Quit();
 	return EXIT_SUCCESS;
@@ -69,6 +139,7 @@ int main(int argc, char* argv[]){
 
 void SDL_ExitWithError(const char* message){
 	SDL_Log("Error %s > %s\n", message, SDL_GetError());
+	TTF_Quit();
 	SDL_Quit();
 	exit(EXIT_FAILURE);
 }
